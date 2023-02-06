5579b423dedd0a19119c830d3af38239c58e694e
ezz2003 <ezz2003@yandex.ru>
2023-02-07 08:32:15 +1000

    input - output in C#

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет                                   ");
+Console.WriteLine(username);
+Console.Write("Привет ");
+Console.Write(username);
