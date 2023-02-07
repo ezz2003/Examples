80ee7396c0c7a5b9716c2cb6f0f3a9539e4071c7
ezz2003 <ezz2003@yandex.ru>
2023-02-07 13:54:01 +1000

    added in DIV double

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,6 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 int numberA = 10;
-int numberb = 2;
+int numberb = 5;
 Console.WriteLine(numberA / numberb);
+
+int numberC = 12;
+int numberD = 5;
+Console.WriteLine(numberC / numberD);
+
+double numberE = 12;
+double numberF = 5;
+Console.WriteLine(numberE / numberF);
 
