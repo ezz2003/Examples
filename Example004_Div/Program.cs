f09e2acfec303c85ce2eb4456b9028b41ea5364f
ezz2003 <ezz2003@yandex.ru>
2023-02-07 11:33:09 +1000

    add folder divide

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int numberA = 10;
+int numberb = 2;
+Console.WriteLine(numberA / numberb);
+
