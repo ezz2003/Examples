2afe665a2866ea357f4a9f010d2c17cfae4eefe7
ezz2003 <ezz2003@yandex.ru>
2023-02-07 14:31:50 +1000

    add random numbers

--- Examle003_Sum/Program.cs
+++ Examle003_Sum/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
-int numberA = 3;
-int numberB = 2;
-Console.Write(numberA + numberB);
+// int numberA = 3;
+// int numberB = 2;
+// Console.Write(numberA + numberB);
+
+int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+int result = numberA + numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(result);
 
